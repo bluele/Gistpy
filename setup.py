710954cc127b807bc447450629e1b47e42e68375
Jun Kimura <jksmphone@gmail.com>
2012-06-12 13:11:16 +0900

    Change version to 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_file(filename):
 
 
 setup(name="Gistpy",
-      version="0.1",
+      version="0.1.0",
       description="Command line client for gist.",
       long_description=read_file("README.rst"),
       license="MIT",
