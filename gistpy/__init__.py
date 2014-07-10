bc6379dbddc3cb24de1b2fd3a864499dea19153b
Jun Kimura <jksmphone@gmail.com>
2014-07-10 17:32:49 +0900

    Fixed module entry_point and updated to version 0.2.1

--- gistpy/__init__.py
+++ gistpy/__init__.py
@@ -5,3 +5,7 @@ from gistpy.auth import Auth
 from gistpy.query import Query
 from gistpy.dispatch import Dispatcher
 from gistpy.argbuilder import ParserBuilder
+
+
+def main():
+    Dispatcher().dispatch()
--- setup.py
+++ setup.py
@@ -17,29 +17,31 @@ def read_file(filename):
         return open(path).read()
 
 
-setup(name="Gistpy",
-      version="0.2.0",
-      description="Command line client for gist.",
-      long_description=read_file("README.rst"),
-      license="MIT",
-      author="Jun Kimura",
-      author_email="junkxdev@gmail.com",
-      url="https://github.com/bluele/Gistpy",
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Topic :: System :: Shells',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-      ],
-      packages = find_packages(),
-      keywords= "cli client gist github sourcecode",
-      install_requires=install_requires,
-      scripts=[
-        join(cwd, "scripts/gistpy"),
-      ],
-      zip_safe = True)
+setup(
+    name="Gistpy",
+    version="0.2.1",
+    description="Command line client for gist.",
+    long_description=read_file("README.rst"),
+    license="MIT",
+    author="Jun Kimura",
+    author_email="jksmphone@gmail.com",
+    url="https://github.com/bluele/Gistpy",
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Topic :: System :: Shells',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(),
+    keywords="cli client gist github sourcecode",
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            'gistpy = gistpy:main'
+    },
+    zip_safe=True)
 
